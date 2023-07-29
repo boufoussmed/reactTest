d4112a9a88e4caca92c307c4e946498f7dfc2df8
boufoussmed <boufoussmohamed@gmail.com>
2023-07-29 11:50:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(req)
+    res.send({ hi: 'there' })
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, rep => {
+    console.log('listening on port', rep);
+});
